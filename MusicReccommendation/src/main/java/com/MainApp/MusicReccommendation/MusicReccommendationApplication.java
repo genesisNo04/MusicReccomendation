5cdb6e226e3a45b238f5e746f18af5f32608ff40
namnguyen <namhoanghainguyen@gmail.com>
2024-11-10 07:50:05 -0700

    first commit

---
+++ MusicReccommendation/src/main/java/com/MainApp/MusicReccommendation/MusicReccommendationApplication.java
@@ -0,0 +1,13 @@
+package com.MainApp.MusicReccommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicReccommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicReccommendationApplication.class, args);
+	}
+
+}
---
+++ MusicReccommendation/src/test/java/com/MainApp/MusicReccommendation/MusicReccommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MainApp.MusicReccommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicReccommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
